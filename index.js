52a1084cc995a6967508cc806f0a8147015aa26f
Nenad Bogojevic <nbogojevic@outlook.com>
2013-03-11 23:29:04 +0100

    Support for uploding scripts for groovy

--- index.js
+++ index.js
@@ -1,39 +1,88 @@
-﻿var spawn = require('child_process').spawn;
+﻿var child_process = require('child_process')
+  , fs = require('fs')
+  , temp = require('temp');
+  
 var config = {
-  python: {
-    cmd: 'D:\\Python33\\python.exe',
-    args: ['-u', '-i']
-    },
-  groovy: {
-    cmd: 'C:\\windows\\system32\\cmd.exe',
-    args: ['/c', 'D:\\java\\groovy-1.7.5\\bin\\groovysh.bat']
-  },
-  debug: false
+  debug: true
 };
 
-var replList = {
+pythonSetup();
+groovySetup();
+
+function pythonSetup() {
+  config.python = {
+    cmd: 'D:\\Python33\\python.exe',
+    argsREPL: ['-u', '-i'],
+    argsFile: []
+  };
+}
+
+function groovySetup() {
+	if (process.env['JAVA_HOME'] != null) {
+		if (process.env['GROOVY_HOME'] != null) {
+			config.groovy = {
+				cmd: process.env['ComSpec'],
+				argsFile: [
+          '/C',
+					process.env['GROOVY_HOME']+'/bin/groovy.bat', 
+					],
+				
+			};
+		}
+		else {
+			console.error('GROOVY_HOME environment variable is not set.');
+		}
+	}
+	else {
+		console.error('JAVA_HOME environment variable is not set.');
+	}
+}
+
+console.log(config);
+
+var scriptSupport = {
   list: [],
-  configure: function (repl) {
-    app.get('/' + repl + '/:cmd', function(req, res) {
-      replList.init(repl, null);
-      replList.send(req, res, repl);
-    });
-    io.of('/'+repl).on('connection', function (socket) {
-      console.log('Connection to ' + repl + ' REPL.');
-      replList.init(repl, socket);
-      socket.on('cmd', function (data) {
-        // console.log("Received: " + data);
-        var cmd = data.cmd;
-        replList.list[repl].app.stdin.write(cmd);
+  configure: function (repl,interactive) {
+	if (interactive) {
+		app.get('/cmd/' + repl + '/:cmd', function(req, res) {
+		  scriptSupport.init(repl, null);
+		  scriptSupport.send(req, res, repl);
+		});
+		io.of('/'+repl).on('connection', function (socket) {
+		  console.log('Connection to %s REPL.', repl);
+		  scriptSupport.init(repl, socket);
+		  socket.on('cmd', function (data) {
+			if (config.debug) console.log("Received: %s %j" , repl, data);
+			var cmd = data.cmd;
+			scriptSupport.list[repl].app.stdin.write(cmd);
+		  });
+		});
+	}
+	app.post('/run/' + repl, function(req, res) {
+    temp.open({suffix: '.'+repl}, function(err, info) {
+      fs.write(info.fd, req.body);
+      console.log(req.body);
+      fs.close(info.fd, function(err) {
+        console.log(info.path);
+        var args = config[repl].argsFile.concat(info.path);
+        child_process.execFile(config[repl].cmd, args, {}, function(error, stdout, stderr) {
+          console.log('stdout: ' + stdout);
+          console.log('stderr: ' + stderr);
+          if (error !== null) {
+            console.log('exec error: ' + error);
+          }
+          res.send(stdout);
+        });
       });
     });
+	});
   },
   send: function (req, res, repl) {
-    if (replList.list[repl] != null) {
-      replList.list[repl].app.stdin.write(req.params.cmd);
-      replList.list[repl].app.stdin.write('\r\n');
-      if (replList.list[repl].socket != null) {
-        replList.list[repl].socket.emit('stdin', req.params.cmd);
+    if (scriptSupport.list[repl] != null) {
+      scriptSupport.list[repl].app.stdin.write(req.params.cmd);
+      scriptSupport.list[repl].app.stdin.write('\r\n');
+      if (scriptSupport.list[repl].socket != null) {
+        scriptSupport.list[repl].socket.emit('stdin', req.params.cmd);
         res.send({status:'ok'});
       }
       else {
@@ -45,24 +94,26 @@ var replList = {
     }
   },
   init: function (repl, socket) {
-    if (replList.list[repl] == null) {
-      replList.list[repl] = {app: spawn(config[repl].cmd, config[repl].args) };
+    if (scriptSupport.list[repl] == null) {
+      scriptSupport.list[repl] = {app: child_process.spawn(config[repl].cmd, config[repl].argsREPL) };
+	  process.stdin.resume();
+	  process.stdin.setEncoding('utf8');
       if (config.debug) {
-        replList.list[repl].app.stdout.on('data', function (data) {
-          console.log('Stdout: ' + data);
+        scriptSupport.list[repl].app.stdout.on('data', function (data) {
+          console.log('Stdout: %s %j', repl, data);
         });
 
-        replList.list[repl].app.stderr.on('data', function (data) {
-          console.log('Stderr: ' + data);
+        scriptSupport.list[repl].app.stderr.on('data', function (data) {
+          console.log('Stdout: %s %j', repl, data);
         });
       }
-      replList.list[repl].app.on('exit', function (code) {
-        console.log('child process ' + repl + 'exited with code ' + code);
-        replList.list[repl] = null;
+      scriptSupport.list[repl].app.on('exit', function (code) {
+        console.error('child process %s exited with code %d', repl, code);
+        scriptSupport.list[repl] = null;
       });
       if (socket != null) {
-        replList.list[repl].socket = socket;
-        replList.pipeStreams(repl);
+        scriptSupport.list[repl].socket = socket;
+        scriptSupport.pipeStreams(repl);
       }
     }
   },
@@ -72,8 +123,8 @@ var replList = {
    * app 'stderr' -> socket 'stderr'
    */
   pipeStreams: function (repl) {
-    var app = replList.list[repl].app;
-    var socket = replList.list[repl].socket;
+    var app = scriptSupport.list[repl].app;
+    var socket = scriptSupport.list[repl].socket;
     var proc = app.process;
     // TODO: This forces proc output to be interpreted as UTF-8. Should send binary data to client and let them deal with it
     app.stdout.setEncoding('utf8');
@@ -108,11 +159,8 @@ app.get('/shutdown', function(req, res) {
   process.exit(1);
 });
 
-replList.configure('python');
-replList.configure('groovy');
-
-process.stdin.resume();
-process.stdin.setEncoding('utf8');
+scriptSupport.configure('python', true);
+scriptSupport.configure('groovy', false);
 
 server.listen(3002, function() {
   console.log('listening on port 3002');
